1c82f09b6d18c075f505f3b5b8dc8719a66817d4
Raj Deshmukh <rajdeshmukh282@gmail.com>
2025-08-30 00:59:27 +0530

    connected db

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(express.json());
 app.post("/api/create", (req, res) => {// make a route
   const { url } = req.body
+  console.log(url);
+  
   res.send(nanoid(7))
 });
 app.listen(3000, () => {// listens to port 3000
