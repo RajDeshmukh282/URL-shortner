4565c9d689f88a5fd197edc874f3d6a351ffd1f1
Raj Deshmukh <rajdeshmukh282@gmail.com>
2025-08-30 00:51:01 +0530

    Add nanoid dependency and implement URL ID generation endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 import express from "express";
+import {nanoid} from "nanoid"
 
 const app = express();
-app.get("/", (req, res) => {// make a route
-  res.send("Hello World!");
+app.use(express.json());
+app.post("/api/create", (req, res) => {// make a route
+  const { url } = req.body
+  res.send(nanoid(7))
 });
 app.listen(3000, () => {// listens to port 3000
   console.log("Server is running on port 3000");
